e4b507df747073035273f3cbefe84b1f2d20b50a
griznog <griznog@gmail.com>
2022-01-11 11:44:50 -0600

    Simplifying deploy script.

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,9 @@ function die () {
   exit 1
 }
 
-# Sites we know how to deploy.
-declare -A SITES
-SITES["hpc-user-guide"]="hpc.czbiohub.org:/var/www/html/hpc-user-guide"
-SITES["hpc-admin-guide"]="hpc.czbiohub.org:/var/www/html/hpc-admin-guide"
-
-# See what site we are working in.
-SITE=$(basename $PWD)
-[[ -n ${SITES[${SITE}]} ]] || die "Unknonwn site ${SITE}"
-
 # Make the site.
 foliant make site || die "Failed running 'foliant make site'"
 
 # Push site to hpc.czbiohub.org
-rsync -rv --progress ${SITE}.mkdocs/ ${SITES[${SITE}]}
+rsync -rv --progress --delete hpc-user-guide.mkdocs/ hpc.czbiohub.org:/var/www/html/
 
