0d0da599d9cf0eb9fb78ba291c5f922934304fae
griznog <griznog@gmail.com>
2022-05-05 09:15:49 -0500

    This is not generalized for many sites anymore.

--- serve.sh
+++ serve.sh
@@ -6,18 +6,8 @@ function die () {
 }
 
 
-# Sites we know how to deploy.
-declare -A SITES
-SITES["hpc-user-guide"]="hpc.czbiohub.org:/var/www/html/hpc-user-guide"
-SITES["hpc-admin-guide"]="NONE"
-
-# See what site we are working in.
-SITE=$(basename $PWD)
-[[ -n ${SITES[${SITE}]} ]] || die "Unknonwn site ${SITE}"
-
-
 while foliant make mkdocs; do
-  pushd ${SITE}.mkdocs.src
+  pushd hpc-user-guide.mkdocs.src
     mkdocs serve || exit 1
   popd
 done
